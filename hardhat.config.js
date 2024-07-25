fb640e5a7f74dab2f90c421a7a735486560793d4
alif fahrizal <aliffahrijal24@gmail.com>
2024-07-25 07:25:50 +0000

    swisstronik quest 1

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
-      accounts: ["0xb76e8175b35d55282385f1623b5ac88a02d2839fd408bf95eb12ebb0b745"], //Your private key starting with "0x" 
+      accounts: ["0xb76e8175b35d55282385f1623b5ac88a02d2839fd408bf95eb12ebb0bcc"], //Your private key starting with "0x" 
       //Make sure you have enough funds in this wallet to deploy the smart contract
     },
   },
